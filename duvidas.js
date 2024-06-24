2878d2137c22e815476dddac70e545b5cc186e43
cw <130410896+leandrocw@users.noreply.github.com>
2024-06-24 14:47:16 -0300

    nova pagina de perguntas e footer novo

--- duvidas.js
+++ duvidas.js
@@ -1,25 +1,19 @@
-
 window.sr = ScrollReveal({ reset: true });
 
-sr.reveal("header", { duration: 1000 });
+sr.reveal(".ticketpurchases", { duration: 1000 })
 
-sr.reveal("main", { duration: 2000 });
+const elements = document.querySelectorAll(".ticketpurchases");
 
-sr.reveal(".ticketpurchases", { duration: 2000 })
+elements.forEach(function (duvida) {
+    duvida.addEventListener("click", function () {
+        duvida.classList.toggle("ativa");
+    });
+});
 
-const elements = document.querySelectorAll('.ticketpurchases')
+const ElementHidden = document.querySelectorAll(".ticketpurchases");
 
-elements.forEach(function (duvida){
-    duvida.addEventListener('click', function(){
-        duvida.classList.toggle('ativa')
-    })
-})
-
-
-const ElementHidden = document.querySelectorAll('.ticketpurchases')
-
-ElementHidden.forEach(function (purchases){
-    purchases.addEventListener('click', function(){
-        purchases.classList.toggle('some')
-    })
-})
+ElementHidden.forEach(function (purchases) {
+    purchases.addEventListener("click", function () {
+        purchases.classList.toggle("some");
+    });
+});
